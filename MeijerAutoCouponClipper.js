5c52cef39d5e08eb4829d50e0dc35b0245eb3580
RileyXX <49823202+RileyXX@users.noreply.github.com>
2023-04-13 07:47:41 -0500

    Update MeijerAutoCouponClipper.js

--- MeijerAutoCouponClipper.js
+++ MeijerAutoCouponClipper.js
@@ -1,5 +1,5 @@
+// Get all the filter elements and loop through them to check if any of them contains a category
 const filterElements = document.querySelectorAll('.coupon-filter__checkbox-wrapper .mperks-checkbox__container');
-
 let checkedCategories = false;
 for (const element of filterElements) {
   if (categories.some((category) => element.textContent.includes(category))) {
@@ -8,69 +8,72 @@ for (const element of filterElements) {
   }
 }
 
-// if there are any coupons available in selected categories
+// If there are coupons available in selected categories, clip them and show a modal with the number of clipped coupons
 if (checkedCategories) {
   setTimeout(() => {
-    //Select all elements with class name 'coupon-tile__button--clip'
+    // Get all the clip elements, the number of coupons already clipped, and calculate how many more coupons need to be clipped
     const clipElements = document.getElementsByClassName('coupon-tile__button--clip');
-    // get the number of clipped coupons from the clipped coupons tab
-    const clipped = document.getElementById('js-clipped-coupon-count-display').innerHTML;
-    // calculate the number of coupons that need to be clipped
-    const numToClip = maxCoupons - Number(clipped);
-    //loop through the number of coupons to clip
+    const preClipped = Number(document.getElementById('js-clipped-coupon-count-display').innerHTML);
+    let numClipped = preClipped;
+    const numToClip = maxCoupons - numClipped;
+
+    // Loop through the clip elements and click them until the maximum number of coupons is reached
     for (let i = 0; i < numToClip; i++) {
       clipElements[i].click();
+      numClipped++;
     }
-  }, 6000); // 6 seconds
 
-  setTimeout(() => {
-    // re-calculate numToClip
-    const clipped = document.getElementsByClassName('coupon-tile__button--unclip').length;
-    const numClipped = Number(clipped);
-    
-    // create a modual/popup
-    const modual = document.createElement('div');
-    modual.setAttribute('id', 'coupon-modual');
-    modual.innerHTML = `
-      <div class="coupon-modual__content">
-        <p>Coupons Clipped: ${numClipped}</p>
-        <p>Clipping Done!</p>
-        <button class="coupon-modual__close-btn">Close</button>
-      </div>
-    `;
-    document.body.appendChild(modual);
-    // add styles to modual
-    modual.style.cssText = `
-      position: fixed;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      background-color: white;
-      padding: 20px;
-      border-radius: 10px;
-      box-shadow: 0px 0px 10px #333333;
-    `;
+    setTimeout(() => {
+      // Calculate the number of newly clipped coupons and the total number of clipped coupons
+      const clipped = numClipped - preClipped;
+      const totalClipped = numClipped;
 
-    const closeBtn = document.querySelector('.coupon-modual__close-btn');
-    closeBtn.addEventListener('click', () => {
-      modual.remove();
-    });
-  }, 10000); // 11 seconds (6 seconds for clipping + 5 seconds delay)
+      // Create a modal with information about the clipped coupons
+      const modal = document.createElement('div');
+      modal.setAttribute('id', 'coupon-modal');
+      modal.innerHTML = `
+        <div class="coupon-modal__content">
+          <p>Coupons Clipped: ${clipped}</p>
+          <p>Total Coupons Clipped: ${totalClipped}</p>
+          <p>Clipping Done!</p>
+          <button class="coupon-modal__close-btn">Close</button>
+        </div>
+      `;
+      document.body.appendChild(modal);
+
+      // Style the modal and add an event listener to remove it when the "Close" button is clicked
+      modal.style.cssText = `
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: white;
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0px 0px 10px #333333;
+      `;
+
+      const closeBtn = document.querySelector('.coupon-modal__close-btn');
+      closeBtn.addEventListener('click', () => {
+        modal.remove();
+      });
+    }, 2000); // Wait 2 seconds for the coupons to be clipped before showing the modal
+  }, 6000); // Wait 6 seconds for the coupons to load before clipping them
 } else {
-  // run when there are no coupons available in selected categories
+  // If there are no coupons available in selected categories, show a modal with a message with a 2 second delay
   setTimeout(() => {
-    // create a modual/popup
-    const modual = document.createElement('div');
-    modual.setAttribute('id', 'coupon-modual');
-    modual.innerHTML = `
-      <div class="coupon-modual__content">
+    // create a modal/popup
+    const modal = document.createElement('div');
+    modal.setAttribute('id', 'coupon-modal');
+    modal.innerHTML = `
+      <div class="coupon-modal__content">
         <p>No coupons available in selected categories</p>
-        <button class="coupon-modual__close-btn">Close</button>
+        <button class="coupon-modal__close-btn">Close</button>
       </div>
     `;
-    document.body.appendChild(modual);
-    // add styles to modual
-    modual.style.cssText = `
+    document.body.appendChild(modal);
+    // Style the modal and add an event listener to remove it when the "Close" button is clicked
+    modal.style.cssText = `
       position: fixed;
       top: 50%;
       left: 50%;
@@ -81,11 +84,9 @@ if (checkedCategories) {
       box-shadow: 0px 0px 10px #333333;
     `;
 
-    // select the close button 
-    const closeBtn = document.querySelector('.coupon-modual__close-btn');
-    // add event listener to close button to remove the modual
+    const closeBtn = document.querySelector('.coupon-modal__close-btn');
     closeBtn.addEventListener('click', () => {
-      modual.remove();
+      modal.remove();
     });
   }, 2000); // 2 seconds delay
 }
